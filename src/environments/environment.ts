c603216a63d57ef23bc3c2a226474e772211f5b7
Alumnos <alumnos@H51-010.local>
2025-05-09 08:34:11 -0500

    Primer commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  // In real lifecycles, this file should not be part of version control
+  production: true,
+  // Server Base URL for REST API
+  serverBaseUrl: 'https://681dfbc3c1c291fa66327e3b.mockapi.io/api/v1',
+  coursesEndpointPath: '/courses',
+};
